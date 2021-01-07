df114cb8262a1e9ea370ab96beb169cf7fd3a5d8
Sasha_MOAP <oleksandr.riabchuk@skoda-auto.cz>
2021-01-07 15:49:22 +0100

    Initial Commit

---
+++ Simpsons methods/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Simpsons methods
+//
+//  Created by Sasha Riabchuk on 07.01.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
